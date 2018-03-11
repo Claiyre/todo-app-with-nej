d13da3e4cbf433d4d82d319d1609042b05861693
Claiyre <ma_yingying2@163.com>
2018-03-11 14:30:09 +0800

    front-end CURD with ajax

--- webapp/js/pro/index.js
+++ webapp/js/pro/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: mayingying 
+ * @Date: 2018-03-11 13:46:43 
+ * @Last Modified by: mayingying
+ * @Last Modified time: 2018-03-11 14:27:33
+ */
+
 var f=function(_tol, _tab, util){
     var _ = NEJ.P,
         _e = _('nej.e'),    // 节点接口
@@ -7,7 +14,7 @@ var f=function(_tol, _tab, util){
         _p = _('nej.ut')
     var allList = []
     var _tabIndex, targetList = [], _listTpl = _e._$addHtmlTemplate('tab-list', true)
-
+    var _element = _e._$get('list')
     // 封装的实用函数
     var ajax = function (url, method, params){
         return new Promise((resolve, reject) => {
@@ -30,6 +37,10 @@ var f=function(_tol, _tab, util){
         })
         
     }
+    ajax('/getList', 'get').then((res) => {
+        allList = res.list
+        changeListView()
+    })
     //解析模板
     _e._$parseTemplate('template-box')
     // 使用tab控件
@@ -42,7 +53,6 @@ var f=function(_tol, _tab, util){
     })._$go(0)    //default to all list
 
     function changeListView(){
-        var _element = _e._$get('list')
         switch(_tabIndex){
         case 0:
             targetList = allList
@@ -55,48 +65,88 @@ var f=function(_tol, _tab, util){
             break
         }
         _element.innerHTML = _e._$getHtmlTemplate(_listTpl, {todoList: targetList})
-        var checks = _e._$getByClassName(_element, 'check-done')
-        var deleteBtns = _e._$getByClassName(_element, 'delete')
-
-        for(var i = 0; i < checks.length; i++) {
-            // 绑定选中/不选中事件
-            _v._$addEvent(checks[i], 'click', function(_event){
-                var index = _e._$attr(_event.target, 'index')
-                index = parseInt(index)
-                allList.map(function(item) {
-                    if(item.id === index) {
-                        item.done = _event.target.checked
-                    }
-                })
-                changeListView()
-            })
-            // 绑定删除事件
-            _v._$addEvent(deleteBtns[i], 'click', function(_event) {
-                var index = _e._$attr(_event.target, 'index')
-                index = parseInt(index)
-                allList = allList.filter((item) => item.id !== index)
-                changeListView()
-            })
-        }
     }
     
     // 绑定enter事件
     _v._$addEvent('input', 'keyup', function(_event) {
         if (parseInt(_event.keyCode) !== 13) return
         var target = _event.target
-        allList.unshift({
+        var param = {
             text: target.value,
             done: false,
-            id: allList.length
-        })
+            id: allList[0] ? allList[0].id + 1 : 0
+        }
         target.value = ''
-        changeListView()
+        ajax('/addList', 'post', param).then((res) => {
+            if(res.code === 200) {
+                allList.unshift(param)
+                changeListView()
+            } else {
+                alert(res.msg)
+            }
+        })
     }, false)
+
+    // 删除、编辑、选中等事件
+    _v._$addEvent(_element, 'click', function(_event) {
+        var className = _event.target.className
+        var index = _e._$attr(_event.target, 'index')
+        index = parseInt(index)
+        var param = {id: index}, url, list
+        console.log('change: ',className, param)
+        if(className === 'delete') {
+            //delete todo
+            url = '/delete'
+        } else {
+            // change todo
+            url = '/change'
+            for(var i = 0; i < allList.length; i++) {
+                list = allList[i]
+                if(list.id === index){
+                    param.text = list.text
+                    param.done = list.done
+                    break
+                }
+            }
+            if(className === 'check-done') {
+                param.changeKey = 'done'
+                param.nowValue = _event.target.checked
+            } else if(className === 'edit-btn') {
+                param.changeKey = 'text'
+                param.nowValue = ''
+            }
+        }
+        ajax(url, 'post', param).then((res) => {
+            if(res.code !== 200) {
+                alert(res.msg)
+            } else if(className === 'delete') {
+                allList = allList.filter((item) => item.id !== index)
+                changeListView()
+            } else {
+                for(var i = 0; i < allList.length; i++) {
+                    list = allList[i]
+                    if(list.id === index){
+                        list[param.changeKey] = param.nowValue
+                        break
+                    }
+                }
+                changeListView()
+            }
+        })
+
+    })
 
     
     _v._$addEvent('delete-all', 'click', function() {
-        allList = []
-        changeListView()
+        ajax('/delete', 'post', {all: true}).then((res) => {
+            if(res.code === 200) {
+                allList = []
+                changeListView()
+            } else {
+                alert(res.msg)
+            }
+        })
+        
     })
 
 
